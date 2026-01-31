fe82c994d40c85669855580d15160f87d7d294e1
Minho Kang <busster@naver.com>
2026-01-31 23:50:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/domino/DominoSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.domino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominoSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominoSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domino/DominoSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominoSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
