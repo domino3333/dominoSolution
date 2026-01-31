2b65dc6a753f0d12fd39fc46aaf1dd35dddaffab
Minho Kang <busster@naver.com>
2026-02-01 00:44:10 +0900

    mybatis 세팅 완료

---
+++ src/main/java/com/domino/mapper/MemberMapper.java
@@ -0,0 +1,9 @@
+package com.domino.mapper;
+
+public interface MemberMapper {
+//	public void create(Board board) throws Exception;
+//	public Board read(Integer boardNo) throws Exception;
+//	public void update(Board board) throws Exception;
+//	public void delete(Integer boardNo) throws Exception;
+//	public List<Board> list() throws Exception;
+}
