405fca3f47f222093b029e5ebe74e39b282d5e9e
Minho Kang <busster@naver.com>
2026-02-01 14:37:09 +0900

    Member 도메인 생성

---
+++ src/main/java/com/domino/domain/Member.java
@@ -0,0 +1,16 @@
+package com.domino.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+	private long no;
+	private String id;
+	private String pw;
+	private String name;
+	private String age;
+	private String birthday;
+	private String gender;
+	
+}
